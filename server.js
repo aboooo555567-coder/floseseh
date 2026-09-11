5a0864b209e4863beadaf6d8b331f190a4b63473
Zakaria <zakariamohammedalisaif-beep@example.com>
2026-09-12 02:04:35 +0300

    Replace SVG mesh with perfectly matched geometry from original Seha report

--- server.js
+++ server.js
@@ -1099,8 +1099,8 @@ app.post('/api/generate-native-pdf', async (req, res) => {
   </div>
   
   <!-- Header: Geometric graphic (right) -->
-  <svg width="180" height="85" viewBox="0 0 250 120" style="position:absolute;top:25px;right:0px;opacity:0.7;">
-    <path d="M 20,30 L 60,0 M 20,30 L 70,60 M 60,0 L 70,60 M 60,0 L 100,0 M 70,60 L 100,0 M 70,60 L 120,40 M 100,0 L 120,40 M 70,60 L 130,100 M 120,40 L 130,100 M 100,0 L 160,0 M 120,40 L 160,0 M 120,40 L 170,50 M 160,0 L 170,50 M 130,100 L 170,50 M 160,0 L 200,0 M 170,50 L 200,0 M 170,50 L 200,80 M 200,0 L 200,80 M 200,0 L 230,30 M 200,80 L 230,30 M 230,30 L 250,0 M 200,80 L 240,80 M 230,30 L 240,80 M 240,80 L 250,0" stroke="#216ba5" stroke-width="0.7" fill="none"/>
+  <svg width="180" height="90" viewBox="0 0 250 110" style="position:absolute;top:20px;right:0px;opacity:0.65;">
+    <path d="M 10,40 L 50,10 L 80,60 L 120,20 L 150,70 L 190,10 L 220,50 L 240,10 L 240,70 L 220,50 M 10,40 L 80,60 L 120,90 L 150,70 L 190,100 L 220,50 M 50,10 L 120,20 L 190,10 L 240,10 M 80,60 L 150,70 L 220,50 M 120,90 L 190,100 L 240,70 M 240,70 L 250,100 L 220,50 M 250,100 L 190,100" stroke="#216ba5" stroke-width="0.75" fill="none"/>
   </svg>
 
   <!-- Horizontal separator line -->
