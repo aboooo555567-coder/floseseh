366d3aa07f3865c4a9220bfd39b191e922ca6a42
Zakaria <zakariamohammedalisaif-beep@example.com>
2026-09-12 02:38:09 +0300

    Replace SVG graphic with custom path that precisely matches the red circle outline

--- server.js
+++ server.js
@@ -1099,8 +1099,8 @@ app.post('/api/generate-native-pdf', async (req, res) => {
   </div>
   
   <!-- Header: Geometric graphic (right) -->
-  <svg width="150" height="80" viewBox="0 0 150 80" style="position:absolute;top:20px;right:0px;opacity:0.6;">
-    <path d="M 0,10 L 40,40 L 90,10 L 130,30 L 150,0 M 40,40 L 60,70 L 90,10 M 60,70 L 130,30 M 90,10 L 110,80 L 130,30 M 110,80 L 150,60" stroke="#b0c4de" stroke-width="1.2" fill="none"/>
+  <svg width="220" height="85" viewBox="0 0 250 100" style="position:absolute;top:20px;right:0px;opacity:0.65;">
+    <path d="M 20,40 L 50,20 L 60,70 Z M 50,20 L 70,0 L 90,40 Z M 50,20 L 90,40 L 60,70 Z M 60,70 L 90,40 L 110,80 Z M 70,0 L 130,0 L 90,40 Z M 90,40 L 130,0 L 150,50 Z M 90,40 L 150,50 L 110,80 Z M 110,80 L 150,50 L 170,90 Z M 130,0 L 190,0 L 150,50 Z M 150,50 L 190,0 L 200,60 Z M 150,50 L 200,60 L 170,90 Z M 190,0 L 240,20 L 200,60 Z M 200,60 L 240,20 L 250,80 Z" stroke="#337ab7" stroke-width="1.0" fill="none"/>
   </svg>
 
   <!-- Horizontal separator line -->
