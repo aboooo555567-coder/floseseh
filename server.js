d8e4a7a47620d4af214be31f9ece7121be28754c
Zakaria <zakariamohammedalisaif-beep@example.com>
2026-09-12 02:16:39 +0300

    Refine geometric SVG mesh structure to precisely match official report visual pattern

--- server.js
+++ server.js
@@ -1099,8 +1099,8 @@ app.post('/api/generate-native-pdf', async (req, res) => {
   </div>
   
   <!-- Header: Geometric graphic (right) -->
-  <svg width="180" height="90" viewBox="0 0 250 110" style="position:absolute;top:20px;right:0px;opacity:0.65;">
-    <path d="M 10,40 L 50,10 L 80,60 L 120,20 L 150,70 L 190,10 L 220,50 L 240,10 L 240,70 L 220,50 M 10,40 L 80,60 L 120,90 L 150,70 L 190,100 L 220,50 M 50,10 L 120,20 L 190,10 L 240,10 M 80,60 L 150,70 L 220,50 M 120,90 L 190,100 L 240,70 M 240,70 L 250,100 L 220,50 M 250,100 L 190,100" stroke="#216ba5" stroke-width="0.75" fill="none"/>
+  <svg width="200" height="100" viewBox="0 0 250 120" style="position:absolute;top:15px;right:0px;opacity:0.75;">
+    <path d="M 230,0 L 200,40 L 230,90 M 200,0 L 200,100 M 200,40 L 160,60 L 200,100 M 200,0 L 130,10 L 160,60 M 160,60 L 110,80 L 130,10 M 130,10 L 70,50 L 110,80 M 70,50 L 50,0 L 130,10 M 110,80 L 90,100 L 70,50 M 50,0 L 20,40 L 70,50" stroke="#216ba5" stroke-width="0.8" fill="none"/>
   </svg>
 
   <!-- Horizontal separator line -->
