c77bacaf9c2f8ff499696fa4eabbeeebf02da001
Zakaria <zakariamohammedalisaif-beep@example.com>
2026-09-12 02:44:31 +0300

    Refine mesh graphic styling, thickness and geometric layout to perfectly match red circle reference

--- server.js
+++ server.js
@@ -1099,8 +1099,8 @@ app.post('/api/generate-native-pdf', async (req, res) => {
   </div>
   
   <!-- Header: Geometric graphic (right) -->
-  <svg width="220" height="85" viewBox="0 0 250 100" style="position:absolute;top:20px;right:0px;opacity:0.65;">
-    <path d="M 20,40 L 50,20 L 60,70 Z M 50,20 L 70,0 L 90,40 Z M 50,20 L 90,40 L 60,70 Z M 60,70 L 90,40 L 110,80 Z M 70,0 L 130,0 L 90,40 Z M 90,40 L 130,0 L 150,50 Z M 90,40 L 150,50 L 110,80 Z M 110,80 L 150,50 L 170,90 Z M 130,0 L 190,0 L 150,50 Z M 150,50 L 190,0 L 200,60 Z M 150,50 L 200,60 L 170,90 Z M 190,0 L 240,20 L 200,60 Z M 200,60 L 240,20 L 250,80 Z" stroke="#337ab7" stroke-width="1.0" fill="none"/>
+  <svg width="230" height="90" viewBox="0 0 250 100" style="position:absolute;top:15px;right:0px;opacity:0.65;">
+    <path d="M 10,45 L 35,15 L 45,65 Z M 35,15 L 75,5 L 85,40 Z M 35,15 L 85,40 L 45,65 Z M 45,65 L 85,40 L 105,80 Z M 75,5 L 125,0 L 85,40 Z M 85,40 L 125,0 L 145,45 Z M 85,40 L 145,45 L 105,80 Z M 105,80 L 145,45 L 165,85 Z M 125,0 L 185,5 L 145,45 Z M 145,45 L 185,5 L 195,55 Z M 145,45 L 195,55 L 165,85 Z M 185,5 L 235,15 L 195,55 Z M 195,55 L 235,15 L 250,85 Z" stroke="#7ca9c9" stroke-width="0.7" fill="none" stroke-linejoin="round"/>
   </svg>
 
   <!-- Horizontal separator line -->
