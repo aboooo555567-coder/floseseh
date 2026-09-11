1500fa69101dcfbe6fab9b39695275e19374fc52
Zakaria <zakariamohammedalisaif-beep@example.com>
2026-09-12 02:29:57 +0300

    Restore original native SVG pattern that flawlessly matches the screenshot

--- server.js
+++ server.js
@@ -1099,8 +1099,8 @@ app.post('/api/generate-native-pdf', async (req, res) => {
   </div>
   
   <!-- Header: Geometric graphic (right) -->
-  <svg width="200" height="100" viewBox="0 0 250 120" style="position:absolute;top:15px;right:0px;opacity:0.75;">
-    <path d="M 230,0 L 200,40 L 230,90 M 200,0 L 200,100 M 200,40 L 160,60 L 200,100 M 200,0 L 130,10 L 160,60 M 160,60 L 110,80 L 130,10 M 130,10 L 70,50 L 110,80 M 70,50 L 50,0 L 130,10 M 110,80 L 90,100 L 70,50 M 50,0 L 20,40 L 70,50" stroke="#216ba5" stroke-width="0.8" fill="none"/>
+  <svg width="150" height="80" viewBox="0 0 150 80" style="position:absolute;top:20px;right:0px;opacity:0.6;">
+    <path d="M 0,10 L 40,40 L 90,10 L 130,30 L 150,0 M 40,40 L 60,70 L 90,10 M 60,70 L 130,30 M 90,10 L 110,80 L 130,30 M 110,80 L 150,60" stroke="#b0c4de" stroke-width="1.2" fill="none"/>
   </svg>
 
   <!-- Horizontal separator line -->
