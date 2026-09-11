85e4dfa372adff025e411f02572e5602cb1f34b2
Zakaria <zakariamohammedalisaif-beep@example.com>
2026-09-12 01:28:42 +0300

    Update top-right geometric graphic to match screenshot exactly

--- server.js
+++ server.js
@@ -1099,8 +1099,8 @@ app.post('/api/generate-native-pdf', async (req, res) => {
   </div>
   
   <!-- Header: Geometric graphic (right) -->
-  <svg width="180" height="120" viewBox="0 0 200 120" style="position:absolute;top:15px;right:0px;opacity:0.5;">
-    <path d="M 20,40 L 50,70 L 90,20 L 140,50 L 190,10 M 50,70 L 80,100 L 120,60 L 170,110 L 190,10 M 90,20 L 120,60 L 140,50 M 20,40 L 40,10 L 90,20 M 120,60 L 150,20 L 190,10 M 80,100 L 90,20" stroke="#7ca9c9" stroke-width="1.0" fill="none"/>
+  <svg width="250" height="150" viewBox="0 0 200 120" style="position:absolute;top:0px;right:0px;opacity:0.85;">
+    <path d="M 20,40 L 50,70 L 90,20 L 140,50 L 190,10 M 50,70 L 80,100 L 120,60 L 170,110 L 190,10 M 90,20 L 120,60 L 140,50 M 20,40 L 40,10 L 90,20 M 120,60 L 150,20 L 190,10 M 80,100 L 90,20" stroke="#5D99C6" stroke-width="1.2" fill="none"/>
   </svg>
 
   <!-- Horizontal separator line -->
